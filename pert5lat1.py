05c5cc7fbdf70e3b2fcdec54723e3000542d9c4b
Timmothy James Wijaya <81542887+Khretons@users.noreply.github.com>
2023-02-17 10:53:56 +0700

    Update pert5lat1.py

--- pert5lat1.py
+++ pert5lat1.py
@@ -2,27 +2,40 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Line chart")
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-st.line_chart(chart_data)
+df = load_data(st.secrets["public_gsheets_url"])
 
-st.write("4A: Slider")
-x = st.slider('x')  #this is a widget
-st.write(x, 'squared is', x * x)
+# Print results.
+for row in df.itertuples():
+    # st.write(f"{row.name} has a :{row.pet}:")
 
-st.write("5A: Sidebar")
+# st.write("Line chart")
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['a', 'b', 'c'])
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
+# st.line_chart(chart_data)
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+# st.write("4A: Slider")
+# x = st.slider('x')  #this is a widget
+# st.write(x, 'squared is', x * x)
+
+# st.write("5A: Sidebar")
+
+# # Add a selectbox to the sidebar:
+# add_selectbox = st.sidebar.selectbox(
+#     'How would you like to be contacted?',
+#     ('Email', 'Home phone', 'Mobile phone')
+# )
+
+# # Add a slider to the sidebar:
+# add_slider = st.sidebar.slider(
+#     'Select a range of values',
+#     0.0, 100.0, (25.0, 75.0)
+# )
