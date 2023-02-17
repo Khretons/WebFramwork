ab6cb36b22712b79e93604ad1fcf9a3ed45f77e0
Timmothy James Wijaya <81542887+Khretons@users.noreply.github.com>
2023-02-17 10:58:31 +0700

    Update pert5lat1.py

--- pert5lat1.py
+++ pert5lat1.py
@@ -12,4 +12,5 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
-for row in df.itertuples()
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
